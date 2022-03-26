39adcabae1bc08fad2e3b831716585d7efc2c16d
MFCRepositories <MFCRepositories@users.noreply.github.com>
2022-03-26 16:15:44 +0300

    java.lang klasorü ve diğer classların import edilme durumu

--- MySelf/src/_010_systemClassNedir/Calistir.java
+++ MySelf/src/_010_systemClassNedir/Calistir.java
@@ -5,6 +5,14 @@ package _010_systemClassNedir;
 public class Calistir {
 
 	public static void main(String[] args) {
+
+		/*
+		 * java.lang klasoru içerisinde bulunan (System) gibi classlar harici tüm
+		 * Classlar "import" edilmek zorundadýrlar
+		 * 
+		 * Örnek olarak Scanner classýnýn içinde bulundugu java.util ya da alternatif
+		 * olarak bu þekilde gösterilmelidir.(java.util.Scanner)
+		 */
 		java.util.Scanner scn = new java.util.Scanner(System.in);
 		System.out.print(false);
 
